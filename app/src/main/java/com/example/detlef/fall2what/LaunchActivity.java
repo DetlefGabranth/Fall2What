3f0cad3295124c6fd00998b0e8d73653a59b9188
DetlefGabranth <detlef.n6@gmail.com>
2015-12-16 12:21:32 +0100

    First Commit

---
+++ app/src/main/java/com/example/detlef/fall2what/LaunchActivity.java
@@ -0,0 +1,15 @@
+package com.example.detlef.fall2what;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        }
+    }
